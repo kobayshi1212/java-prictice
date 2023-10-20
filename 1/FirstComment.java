75d882c8268c08ecb91fc97e1742a571b22b5c4f
kobakon <138418858+kobakon@users.noreply.github.com>
2023-10-20 10:56:48 +0000

    fist commit

---
+++ 1/FirstComment.java
@@ -0,0 +1,9 @@
+/* FirstComment. java
+   文字列を表示するプログラム
+      2023.10.20 小林作成            */
+
+class FirstComment {
+  public static void main (String[] args) {  
+    System.out.println("javaを楽しもう！");       // 画面表示
+  }
+}
