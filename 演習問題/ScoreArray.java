561db8fb729435504ee0d7d6e342f4b50a5042f2
kobakon <138418858+kobakon@users.noreply.github.com>
2023-10-21 08:49:07 +0000

    演習問題

---
+++ 演習問題/ScoreArray.java
@@ -0,0 +1,17 @@
+class ScoreArray{
+  public static void main(String[] args){
+    //配列の生成
+    int[] data = new int[4];
+    //配列に値を代入
+    data[0] = 90;
+    data[1] = 80;
+    data[2] = 100;
+    data[3] = 90;
+
+    //値を表示
+    System.out.println("1人は" + data[0] + "点");
+    System.out.println("2人は" + data[1] + "点");
+    System.out.println("3人は" + data[2] + "点");
+    System.out.println("4人は" + data[3] + "点");
+  }
+}
---
+++ 演習問題/ShowData.java
@@ -0,0 +1,17 @@
+class ShowData{
+  public static void main(String[] args){
+    //•Ï”‚ÌéŒ¾‚Æ’l‚Ì‘ã“ü
+    int num = 10;
+    double pi = 3.14;
+    char blood = 'A';
+    boolean fun = true;
+    String msg = "‚¨‚È‚©Œ¸‚Á‚½";
+
+    //’l‚Ì•\¦
+    System.out.println(num);
+    System.out.println(pi);
+    System.out.println(blood);
+    System.out.println(fun);
+    System.out.println(msg);
+  }
+}
